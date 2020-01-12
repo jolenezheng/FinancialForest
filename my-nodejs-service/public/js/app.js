3a24d7638efa31cd80a8eab10346e0994d5e1546
jolenezheng <jolenezheng18@gmail.com>
2020-01-12 01:00:21 -0500

    merged

--- my-nodejs-service/public/js/app.js
+++ my-nodejs-service/public/js/app.js
@@ -14,19 +14,11 @@
 // })
 
 $(document).ready(function() {
-
     $('#upload-btn').click(function() {
-        // using XMLHttpRequest
-        // let xhr = new XMLHttpRequest();
-        // xhr.open("GET", "http://localhost:8080/getData", true); xhr.onload = function () {
-        //     console.log(xhr.responseText);
-        // }; xhr.send();
         $.get('http://localhost:8080/getData', function (data) {
-            // $(".result").html(data);
         })
         .done(function (response) {
             console.log(response);
         })
     })
-
 });
